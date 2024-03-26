fbc7f8fccb89b99537eb0979cb437d9709ca3a61
relmelegy <77593012+relmelegy@users.noreply.github.com>
2024-03-26 07:50:52 +0000

    build

---
+++ build/precache-manifest.5ffed673db37984c080c161fb0a6cfe4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1318e677cd4a5d031274b77af95f199",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26ce6c5457948f3c29a0",
+    "url": "/static/js/2.64477d87.chunk.js"
+  },
+  {
+    "revision": "ae4902a6132bf5705633",
+    "url": "/static/js/main.e209ece2.chunk.js"
+  },
+  {
+    "revision": "641bb0516db3d047d70b",
+    "url": "/static/js/runtime-main.be65498d.js"
+  }
+]);
